cc7c88bb065e06f77e323113aed70805defa9678
levinnimrod <levin.nimrod@gmail.com>
2020-06-18 18:23:41 +0200

    NWL Desktop

--- R Scripts/clustering for the CDDQ typology.R
+++ R Scripts/clustering for the CDDQ typology.R
@@ -1,8 +1,6 @@
 ####################      RELEVANT LIBRARIES AND WORKING DIRECTORY                ####################
 remove(list = ls())
-
-
-library(dplyr)
+library(dplyr); library(mclust)
 
 ####################      LOAD TIDY FILES AND SUBSET FOR THE RELEVANT SAMPLE                ####################
 cddq <- file.choose() %>% read.csv
@@ -12,11 +10,24 @@ cddq <- cddq[!is.na(cddq$RCA) & cddq$Country == "USA", ]
 
 # remove participants with no difference in responses
 url <- file.choose(); source(url)
-cddq <- cddq[!exclude(cddq[, c(49:51, 53:56, 58:60)]) == 0, ]
+cddq <- cddq[!exclude(cddq[, c(49:51, 53:56, 58:60)]) == 0, ]; remove(url, exclude)
 
 ####################      CREATE TWO RANDOM SAMPLES                ####################
 sample1 <- cddq[seq(1, nrow(cddq), by = 2), ]; sample1 <- sample1[, -1]; 
-sample2 <- cddq[seq(2, nrow(cddq), by = 2), ]; sample2 <- sample2[, -1]; 
+sample2 <- cddq[seq(2, nrow(cddq), by = 2), ]; sample2 <- sample2[, -1];
+remove(cddq)
+
+# get only the variables you need
+sample1 <- (sample1[, c(4:5, 8:11, 48:50, 52:55, 57:59, 61)]) %>% round(2)
+sample2 <- (sample2[, c(4:5, 8:11, 48:50, 52:55, 57:59, 61)]) %>% round(2)
+
+####################      RUN MIXTURE MODEL CLUSTERING                ####################
+# create ipsative scores for each participant
+data <- sample1[, 7:16] %>% t %>% scale %>% t %>% round(2) %>% as.data.frame()
+
+# get best estimate of loglikelihood from previous iteration
+url <- file.choose(); source(url)
+result <- typology(data = data, n_groups = 4, sample = 1, iterations = 5)
 
 
 
