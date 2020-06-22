e5ba8584400a00886ad380c7707b32ff3a812f6d
levinnimrod <levin.nimrod@gmail.com>
2020-06-22 14:55:56 +0200

    NWL Desktop

--- R Scripts/Compare two-sample clustering solutions.R
+++ R Scripts/Compare two-sample clustering solutions.R
@@ -26,6 +26,10 @@ remove(cddq)
 sample1 <- (sample1[, c(4:5, 8:11, 48:50, 52:55, 57:59, 61)]) %>% round(2)
 sample2 <- (sample2[, c(4:5, 8:11, 48:50, 52:55, 57:59, 61)]) %>% round(2)
 
+####################      CALCULATE RI SCORE                ####################
+sample1$LI <- (sample1$Lp + sample1$Lo + sample1$Lp + sample1$Ls) / 4 %>% round(2)
+sample2$LI <- (sample2$Lp + sample2$Lo + sample2$Lp + sample2$Ls) / 4 %>% round(2)
+
 ####################      EXCTRACT CLASSIFICATIONS FROM THE SOLUTIONS                ####################
 classifications = data.frame(rep(NA, nrow(ldf[[1]])))
 
@@ -36,42 +40,65 @@ for (i in seq(18)){
 classifications <- cbind(classifications[,2:9], classifications[,1], classifications[,11:18], classifications[,10])
 colnames(classifications) <- paste0("S", c(rep(1,9), rep(2, 9)), "_G", seq(2,10))
 
-####################      EVALUATE THE MEAN DIFFERENCES                ####################
+####################      EVALUATE THE MEAN DIFFERENCES - Ri, Rd, LI, Ie                ####################
+names <- c(paste0('S1_G', 2:10), paste0('S2_G', 2:10))
+
+# define which two solutions you want to compare (i = ?)
+i = 7
+
+aggregate(sample1[c('Ri', 'Rd', 'LI', 'Ie')], by = as.data.frame(classifications[names[i-1]]), FUN = mean) 
+
+
+# Extract the mean values of the variables for the clustering groups from sample 1
+results = cbind(aggregate(sample1[c('Ri', 'Rd', 'LI', 'Ie', 'total')], by = as.data.frame(classifications[names[i-1]]), 
+                  FUN = mean) %>% round(2), table(classifications[names[i-1]])) %>% arrange(desc(total)) %>% t
+
+# Extract the mean values of the variables for the clustering groups from sample 2
+results = results %>% cbind(cbind(aggregate(sample2[c('Ri', 'Rd', 'LI', 'Ie', 'total')], by = as.data.frame(classifications[names[i-1+9]]), 
+                   FUN = mean) %>% round(2), table(classifications[names[i-1+9]])) %>% arrange(desc(total)) %>% t) 
+
+
+results[1, ] <- c(rep(1, i), rep(2, i)); results <- t(results) %>% as.data.frame; 
+results <- arrange(results, desc(total)) %>% t %>% as.data.frame(); 
+results
+
+
+####################      EVALUATE THE MEAN DIFFERENCES - ALL 10 DIMENSIONS               ####################
 names <- c(paste0('S1_G', 2:10), paste0('S2_G', 2:10))
 
 # define which two solutions you want to compare (i = ?)
 i = 4
 
-results = cbind(
 # Extract the mean values of the variables for the clustering groups from sample 1
-aggregate(sample1[, 7:17], by = as.data.frame(classifications[names[i-1]]), FUN = mean) %>% round(2) %>%
-       arrange(desc(total)) %>% t,
+results = cbind(aggregate(sample1[, 7:17], by = as.data.frame(classifications[names[i-1]]), 
+                          FUN = mean) %>% round(2), table(classifications[names[i-1]])) %>% arrange(desc(total)) %>% t
 
 # Extract the mean values of the variables for the clustering groups from sample 2
-aggregate(sample2[, 7:17], by = as.data.frame(classifications[names[i - 1 + 9]]), FUN = mean) %>% round(2) %>%
-       arrange(desc(total)) %>% t)   %>% as.data.frame()
+results = results %>% cbind(cbind(aggregate(sample2[, 7:17], by = as.data.frame(classifications[names[i-1+9]]), 
+                                            FUN = mean) %>% round(2), table(classifications[names[i-1+9]])) %>% arrange(desc(total)) %>% t) 
 
-results[1, ] <- c(rep(1, i), rep(2, i));
-results <- t(results) %>% as.data.frame;
-arrange(results, desc(total)) %>% t
 
- ####################      COMPUTE THE MEAN OF THE MAXIMUM DIFFERENCE BETWEEN SAMPLES ACROSS THE 10 SCORES                ####################
+results[1, ] <- c(rep(1, i), rep(2, i)); results <- t(results) %>% as.data.frame; 
+results <- arrange(results, desc(total)) %>% t %>% as.data.frame(); 
+results
+
+# ###################      COMPUTE THE MEAN OF THE MAXIMUM DIFFERENCE BETWEEN SAMPLES ACROSS THE 10 SCORES                ####################
 # results = rep(NA, 12) %>% as.data.frame()
 # 
 # for (i in seq(9)) {
 # 
-# results[, i] = 
+# results[, i] =
 # # Exctract the mean values of the variables for the clustering groups from sample 1
 # (aggregate(sample1[, 7:17], by = as.data.frame(classifications[names[i]]), FUN = mean) %>% round(2) %>%
-#         arrange(desc(total)) - 
+#         arrange(desc(total)) -
 # # then substract the mean values of the variables for the clustering groups from sample 2 (get the absolute values)
 # aggregate(sample2[, 7:17], by = as.data.frame(classifications[names[i + 9]]), FUN = mean) %>%
 #   arrange(desc(total))) %>% abs %>%
-#   
+# 
 #   apply(2, max) %>% round(2)
 # 
 # }
 # results <- results[-1, ] %>% as.data.frame()
 # rownames(results) <- colnames(sample1[, 7:17]); colnames(results) <- c(seq(2, 10))
-# results  
+# results
 # results[1:10,] %>% colMeans() %>% round(2)
