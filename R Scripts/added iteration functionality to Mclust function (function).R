94b130a717cf9c9b8ae39af0806f04cfcf46713b
Nimrod Levin <nimrod.levin@mail.huji.ac.il>
2020-06-22 15:32:51 +0200

    NWL Laptop

--- R Scripts/added iteration functionality to Mclust function (function).R
+++ R Scripts/added iteration functionality to Mclust function (function).R
@@ -18,7 +18,7 @@ typology <- function(data, n_groups, sample, seed = 1) {
   # run mclust to check for better solutions
   while ( (ll[n_groups, sample + 4] != 0) & (i < 200) ) {  
     i = i + 1
-    print(paste0("Iteration #", i, " for sample #", sample, ' and groups = ', n_groups, ' (seed = ', seed))
+    print(paste0("Iteration #", i, " for sample #", sample, ' and groups = ', n_groups, ' (seed = ', seed, ')'))
     result <- Mclust(data, G = n_groups, modelNames = 'EII', initialization = list(hcpairs = randpairs))
     
     if (round(result$loglik,2) >= ll[n_groups, sample]) {
